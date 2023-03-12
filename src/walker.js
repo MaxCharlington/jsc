a9c338fdc52526a3bd28172cde8ae3312363fdbe
MaxCharlington <maxcharlington03@yandex.ru>
2023-03-12 17:44:39 +0300

    Added tests for functions, fixed some issues

--- src/walker.js
+++ src/walker.js
@@ -6,7 +6,7 @@ export default function WalkAST (tree) {
     const cpp_state = {
         functions: []
     };
-    ancestor(tree, handlers, null, {});
+    ancestor(tree, handlers, null, cpp_state);
 
     tree.functions = cpp_state.functions;
 }
---
+++ tests/sources/function/function_statement_local_captures.js
@@ -0,0 +1,7 @@
+function f(){
+    let a = 1;
+    function f1()
+    {
+        return a
+    }
+}
