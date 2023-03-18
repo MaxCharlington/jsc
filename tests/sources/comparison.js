bdb2b44cf382cf40f9c4a5a28d361ae3f8d0c7cf
MaxCharlington <maxcharlington03@yandex.ru>
2023-03-18 03:58:07 +0300

    Added basic support for eq and strict eq operators

---
+++ tests/sources/comparison.js
@@ -0,0 +1,7 @@
+const str = "aasdasd";
+if (str == "aasdasd") console.log(1);
+if (str === "aasdasd") console.log(2);
+
+let str2 = "aasdasd";
+if (str2 == "aasdasd") console.log(3);
+if (str2 === "aasdasd") console.log(4);
