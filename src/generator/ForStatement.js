546f5d4cf326ea18693b5ed2b82cb56048f82222
MaxCharlington <maxcharlington03@yandex.ru>
2023-10-24 15:15:32 +0300

    Added scope for loops to move declarations

--- src/generator/ForStatement.js
+++ src/generator/ForStatement.js
@@ -1,21 +1,22 @@
 const empty = { type: "EmptyStatement" };
 
 export function ForStatement (leaf, toString) {
-    let str = "for (";
+    let str = "{ for (";
     str += toString(leaf.init ?? empty);
     str += toString(leaf.test ?? empty) + ";";
     if (leaf.update) str += toString(leaf.update);
     str += ") ";
     str += toString(leaf.body);
+    str += "}";
     return str;
 }
 
 export function ForInStatement (leaf, toString) {
     const loop_var = leaf.left.declarations[0].id.name;
-    return `DynamicTyping::for_in(${toString(leaf.right)}, [&](auto ${loop_var})${toString(leaf.body)});`;
+    return `{ DynamicTyping::for_in(${toString(leaf.right)}, [&](auto ${loop_var})${toString(leaf.body)}); }`;
 }
 
 export function ForOfStatement (leaf, toString) {
     const loop_var = leaf.left.declarations[0].id.name;
-    return `DynamicTyping::for_of(${toString(leaf.right)}, [&](auto ${loop_var})${toString(leaf.body)});`;
+    return `{ DynamicTyping::for_of(${toString(leaf.right)}, [&](auto ${loop_var})${toString(leaf.body)}); }`;
 }
